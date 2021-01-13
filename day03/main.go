e370ab96af73d1d9e824682921c583f291d26595
Blake Thomson <blake.thomson@voxmedia.com>
2021-01-12 17:12:38 -0800

    day 3, part 2

--- day03/main.go
+++ day03/main.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+const tree = `#`
 
 func readInput() ([]string, error) {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -19,17 +22,61 @@ func readInput() ([]string, error) {
 	return lines, nil
 }
 
+func check1(i int, line string) int {
+	return (i * 3)
+}
+
+func check2(i int, line string) int {
+	return i
+}
+
+func check3(i int, line string) int {
+	return (i * 5)
+}
+
+func check4(i int, line string) int {
+	return (i * 7)
+}
+
+func check5(i int, line string) int {
+	if i%2 == 0 {
+		return i / 2
+	} else {
+		return strings.Index(line, ".")
+	}
+}
+
 func main() {
 	input, err := readInput()
 	if err != nil {
 		panic(err)
 	}
-	count := 0
+	count1 := 0
+	count2 := 0
+	count3 := 0
+	count4 := 0
+	count5 := 0
 	for i, line := range input {
-		char := line[(i*3)%len(line)]
-		if string(char) == "#" {
-			count++
+		if string(line[check1(i, line)%len(line)]) == tree {
+			count1++
+		}
+		if string(line[check2(i, line)%len(line)]) == tree {
+			count2++
+		}
+		if string(line[check3(i, line)%len(line)]) == tree {
+			count3++
+		}
+		if string(line[check4(i, line)%len(line)]) == tree {
+			count4++
+		}
+		if string(line[check5(i, line)%len(line)]) == tree {
+			count5++
 		}
 	}
-	fmt.Println(count)
+	fmt.Println(count1)
+	fmt.Println(count2)
+	fmt.Println(count3)
+	fmt.Println(count4)
+	fmt.Println(count5)
+	fmt.Println(count1 * count2 * count3 * count4 * count5)
 }
