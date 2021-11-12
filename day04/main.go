fb8f9826bc4c006c446af1871a62ed9fc939e6e2
Blake Thomson <blake.thomson@voxmedia.com>
2021-11-12 10:10:30 -0800

    wip

--- day04/main.go
+++ day04/main.go
@@ -4,90 +4,54 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
-type Passport struct {
-	byr int
-	iyr int
-	eyr int
-	hgt string
-	hcl string
-	ecl string
-	pid string
-	cid string
+var exists = struct{}{}
+var requiredKeys = map[string]struct{}{
+	"byr": exists,
+	"iyr": exists,
+	"eyr": exists,
+	"hgt": exists,
+	"hcl": exists,
+	"ecl": exists,
+	"pid": exists,
 }
 
-func (p *Passport) parse(line string) error {
-	tokens := strings.Split(line, " ")
+// Passport tktk
+type Passport map[string]string
+
+func parseLine(p *Passport, line *string) error {
+	tokens := strings.Split(*line, " ")
 	for _, token := range tokens {
 		parts := strings.Split(token, ":")
-		if len(parts) != 2 {
-			return fmt.Errorf("invalid token: %v", token)
-		}
-		switch string(parts[0]) {
-		case "byr":
-			byr, err := strconv.Atoi(parts[1])
-			if err != nil {
-				return fmt.Errorf("expected int for byr: %v", err)
-			} else {
-				p.byr = byr
-			}
-		case "iyr":
-			iyr, err := strconv.Atoi(parts[1])
-			if err != nil {
-				return fmt.Errorf("expected int for iyr: %v", err)
-			} else {
-				p.iyr = iyr
-			}
-		case "eyr":
-			eyr, err := strconv.Atoi(parts[1])
-			if err != nil {
-				return fmt.Errorf("expected int for eyr: %v", err)
-			} else {
-				p.eyr = eyr
-			}
-		case "hgt":
-			p.hgt = parts[1]
-		case "hcl":
-			p.hcl = parts[1]
-		case "ecl":
-			p.ecl = parts[1]
-		case "pid":
-			p.pid = parts[1]
-		case "cid":
-			p.cid = parts[1]
-		default:
-			return fmt.Errorf("invalid token key: %s", parts[0])
-		}
+		(*p)[parts[0]] = (*p)[parts[1]]
 	}
 	return nil
 }
 
 func (p *Passport) isValid() bool {
-	return p.byr != 0 &&
-		p.iyr != 0 &&
-		p.eyr != 0 &&
-		p.hgt != "" &&
-		p.hcl != "" &&
-		p.ecl != "" &&
-		p.pid != ""
+	for k := range requiredKeys {
+		if _, ok := (*p)[k]; !ok {
+			return false
+		}
+	}
+	return true
 }
 
 func readInput() (*[]Passport, error) {
 	scanner := bufio.NewScanner(os.Stdin)
 	var passports []Passport
-	passport := Passport{}
+	passport := make(Passport)
 	parsed := false
 	for scanner.Scan() {
 		line := scanner.Text()
 		if len(line) == 0 {
 			passports = append(passports, passport)
-			passport = Passport{}
+			passport = make(Passport)
 			parsed = false
 		} else {
-			err := passport.parse(line)
+			err := parseLine(&passport, &line)
 			if err != nil {
 				return &passports, err
 			} else {
